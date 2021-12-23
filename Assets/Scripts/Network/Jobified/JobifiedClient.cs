ddbd32958cae49c5fa491dc414108a3425d84fee
Josh Kabo <joshkong1@gmail.com>
2021-12-22 23:12:13 -0800

    start on jobifyu

--- Assets/Scripts/Network/Jobified/JobifiedClient.cs
+++ Assets/Scripts/Network/Jobified/JobifiedClient.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+using Unity.Collections;
+using Unity.Networking.Transport;
+using Unity.Jobs;
 
 public class JobifiedClient : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+}
+
+struct ClientUpdateJob : IJob
+{
+    public NetworkDriver driver;
+    public NativeArray<NetworkConnection> connection;
+    public NativeArray<byte> doneCommunicating;
+
+    public void Execute()
+    {
+        throw new System.NotImplementedException();
     }
 }
