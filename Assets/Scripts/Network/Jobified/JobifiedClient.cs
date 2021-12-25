d144b047b48fb9523248c4376f63d9bb02da931c
HookJabs <joshkong1@gmail.com>
2021-12-24 16:59:00 -0800

    JobifiedClient has a start on the meat of communication

--- Assets/Scripts/Network/Jobified/JobifiedClient.cs
+++ Assets/Scripts/Network/Jobified/JobifiedClient.cs
@@ -19,14 +19,34 @@ public class JobifiedClient : MonoBehaviour
     }
 }
 
+// Job struct
 struct ClientUpdateJob : IJob
 {
     public NetworkDriver driver;
+    // Data is copied, use a sharedcontainer, like NC to access
     public NativeArray<NetworkConnection> connection;
+    // Byte is blittable, bool is not
     public NativeArray<byte> doneCommunicating;
 
     public void Execute()
     {
-        throw new System.NotImplementedException();
+        if (doneCommunicating[0] != 1)
+            Debug.Log("c: Something went wrong during connection");
+        DataStreamReader dataStreamReader;
+        NetworkEvent.Type cmd;
+
+        while ((cmd = connection[0].PopEvent(driver, out dataStreamReader)) != NetworkEvent.Type.Empty)
+        {
+            if (cmd == NetworkEvent.Type.Connect)
+            {
+                Debug.Log("c: now connected to server!");
+
+                uint value = 1;
+                driver.BeginSend(connection[0], out DataStreamWriter dataStreamWriter);
+                dataStreamWriter.WriteUInt(value);
+                driver.EndSend(dataStreamWriter);
+            }
+        }
+        // else if here
     }
 }
