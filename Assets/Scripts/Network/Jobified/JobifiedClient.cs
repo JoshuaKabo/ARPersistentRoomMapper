bedad7df42b546296a49b46411a0d9b25156a9d4
HookJabs <joshkong1@gmail.com>
2021-12-24 17:34:33 -0800

    Jobified Client should be fully implemented

--- Assets/Scripts/Network/Jobified/JobifiedClient.cs
+++ Assets/Scripts/Network/Jobified/JobifiedClient.cs
@@ -6,16 +6,56 @@ using Unity.Jobs;
 
 public class JobifiedClient : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public NetworkDriver m_Driver;
+    // containerized connection and done for reading from job worker
+    public NativeArray<NetworkConnection> m_Connection;
+    public NativeArray<byte> m_DoneCommunicating;
+    // tracks ongoing jobs
+    public JobHandle ClientJobHandle;
+
     void Start()
     {
+        m_Driver = NetworkDriver.Create();
+        // persistent = long as you want, not just a couple frames
+        m_Connection = new NativeArray<NetworkConnection>(1, Allocator.Persistent);
+        m_DoneCommunicating = new NativeArray<byte>(1, Allocator.Persistent);
 
+        NetworkEndPoint endPoint = NetworkEndPoint.LoopbackIpv4;
+        endPoint.Port = 9000;
+
+        m_Connection[0] = m_Driver.Connect(endPoint);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // before running a new frame, ensure last frame was completed
+        // note the perferred use of handle, not driver.scheduleupdate.
+        ClientJobHandle.Complete();
 
+        // job struct instance
+        ClientUpdateJob job = new ClientUpdateJob
+        {
+            driver = m_Driver,
+            connection = m_Connection,
+            doneCommunicating = m_DoneCommunicating
+        };
+
+        // scheduleupdate makes networkdriver give a handle for the job
+        ClientJobHandle = m_Driver.ScheduleUpdate();
+        // now give the handle to the job to a new schedule
+        ClientJobHandle = job.Schedule(ClientJobHandle);
+    }
+
+    private void OnDestroy()
+    {
+        // ensure job is complete before cleaning up data
+        ClientJobHandle.Complete();
+
+        // important to dispose all nativearrays
+        m_Connection.Dispose();
+        m_Driver.Dispose();
+        m_DoneCommunicating.Dispose();
     }
 }
 
@@ -46,7 +86,20 @@ struct ClientUpdateJob : IJob
                 dataStreamWriter.WriteUInt(value);
                 driver.EndSend(dataStreamWriter);
             }
+            else if (cmd == NetworkEvent.Type.Data)
+            {
+                uint value = dataStreamReader.ReadUInt();
+                Debug.Log("c: Got the value " + value + " back from the server!");
+                // set done to true, only accept one transmission
+                doneCommunicating[0] = 1;
+                connection[0].Disconnect(driver);
+                connection[0] = default(NetworkConnection);
+            }
+            else if (cmd == NetworkEvent.Type.Data)
+            {
+                Debug.Log("c: client got d/c'd");
+                connection[0] = default(NetworkConnection);
+            }
         }
-        // else if here
     }
 }
