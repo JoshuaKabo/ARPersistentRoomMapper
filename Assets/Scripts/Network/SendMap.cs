82a150c1188866bb56637e6078f6f2902c0fa520
HookJabs <joshkong1@gmail.com>
2022-01-01 22:54:53 -0800

    Start on some useful networking class creation

---
+++ Assets/Scripts/Network/SendMap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+TODO: This class should be written last, once everything is ready
+*/
+
+public class SendMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
